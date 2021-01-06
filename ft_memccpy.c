ce9d194cd042fcfa9258c301661dfd54deb3b29b
julesfieux <julesfieux@yahoo.fr>
2021-01-06 11:29:30 +0100

    modif memccpy

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: jfieux <jfieux@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/18 17:07:02 by jfieux            #+#    #+#             */
-/*   Updated: 2020/11/25 18:21:38 by jfieux           ###   ########.fr       */
+/*   Updated: 2021/01/06 11:26:47 by jfieux           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,8 @@ void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
 
 	if (!dest && !src)
 		return (0);
-	d = (char *)dest;
-	s = (char *)src;
+	d = (unsigned char *)dest;
+	s = (unsigned char *)src;
 	i = 0;
 	while (i < n)
 	{

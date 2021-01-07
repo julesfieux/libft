5299e0ddb9b5d4fe65fabf25a82d155039edda3f
julesfieux <julesfieux@yahoo.fr>
2021-01-07 16:24:09 +0100

    modif itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: jfieux <jfieux@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/30 15:56:05 by jfieux            #+#    #+#             */
-/*   Updated: 2021/01/05 11:05:13 by jfieux           ###   ########.fr       */
+/*   Updated: 2021/01/07 16:23:48 by jfieux           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 static char	*ft_malres(long int num, char *res, int *i)
 {
+	if (num == 0)
+		(*i)++;
 	if (num < 0)
 	{
 		(*i)++;

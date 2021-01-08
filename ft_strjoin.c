8729f28ca32502f9a9119e2047f547bea03c6350
julesfieux <julesfieux@yahoo.fr>
2021-01-08 16:21:52 +0100

    norm strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,25 +6,16 @@
 /*   By: jfieux <jfieux@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/26 16:24:44 by jfieux            #+#    #+#             */
-/*   Updated: 2021/01/08 16:14:01 by jfieux           ###   ########.fr       */
+/*   Updated: 2021/01/08 16:21:27 by jfieux           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*join_init(char *res, char *s1, char *s2, int i)
 {
-	int		len;
-	int		i;
-	int		j;
-	char	*res;
+	int j;
 
-	if (!s1 || !s2)
-		return (0);
-	len = (ft_strlen(s1) + ft_strlen(s2));
-	if (!(res = malloc(sizeof(char) * (len + 1))))
-		return (0);
-	i = 0;
 	j = 0;
 	while (s1[i])
 	{
@@ -40,5 +31,19 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		j++;
 	}
 	res[j] = '\0';
+	return (res);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int		len;
+	char	*res;
+
+	if (!s1 || !s2)
+		return (0);
+	len = (ft_strlen(s1) + ft_strlen(s2));
+	if (!(res = malloc(sizeof(char) * (len + 1))))
+		return (0);
+	join_init(res, s1, s2, 0);
 	return (res);
 }
